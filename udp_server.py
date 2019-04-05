ea4887859439c98bc9aa648977a75d870d357bf3
oksana-ruderman <43013539+oksana-ruderman@users.noreply.github.com>
2019-04-04 21:33:12 -0700

    Delete udp_server.py

--- udp_server.py
+++
@@ -1,9 +0,0 @@
-from socket import*
-serverPort = 45671
-serverSocket = socket (AF_INET,SOCK_DGRAM)
-serverSocket.bind(('127.0.0.1',serverPort))
-print 'The server is ready to receive'
-while 1:
-	message, clientAddress = serverSocket.recvfrom(2048)
-	modifiedMessage = message.upper()
-	serverSocket.sendto(modifiedMessage, clientAddress)
